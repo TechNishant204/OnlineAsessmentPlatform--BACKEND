bbaaee3c6fe9fac4c4a08b2a7d3dfe5291e51eeb
TechNishant204 <nishantmehto7588@gmail.com>
2025-04-22 20:19:09 +0530

    fix cors error

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import db from "./config/database.js";
-import logger from "./utils/loggerUtils.js";
-import helmet from "helmet";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const db = require("./config/database");
+const logger = require("./utils/loggerUtils");
+const helmet = require("helmet");
 // const cookieParser = require("cookie-parser");
 
 //Import Routes
-const authRoutes = require("./routes/authRoutes.js");
-const examRoutes = require("./routes/examRoutes.js");
-const questionRoutes = require("./routes/questionRoutes.js");
-const resultRoutes = require("./routes/resultRoutes.js");
-const profileRoutes = require("./routes/profileRoutes.js");
+const authRoutes = require("./routes/authRoutes");
+const examRoutes = require("./routes/examRoutes");
+const questionRoutes = require("./routes/questionRoutes");
+const resultRoutes = require("./routes/resultRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 //Load environment variables
 dotenv.config();
@@ -24,13 +24,16 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
+    exposedHeaders: ["Content-Range", "X-Content-Range"],
+    maxAge: 600,
+  })
+);
 
 app.use(express.json());
-app.use(logger.middleware); // use the logger middleware
-app.use(helmet()); // use helmet for security
+app.use(logger.middleware);
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 
 //Use Routes
@@ -44,10 +47,9 @@ app.use("/api/profile", profileRoutes);
 db.connectDB();
 
 app.get("/", (_, res) => {
-  logger.info("Root endpoint accessed"); // Added logging for the root endpoint
+  logger.info("Root endpoint accessed");
   res.send("EduAssess is Running");
 });
-
 
 app.use((err, _, res, _next) => {
   logger.error(`Unhandled error: ${err.message}`, { stack: err.stack });
@@ -55,7 +57,6 @@ app.use((err, _, res, _next) => {
     status: "error",
     message: "Something went wrong!",
   });
-});
 });
 
 // Handle 404 routes
